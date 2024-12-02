1373faf9124eab92a2ae2ba8563bc838973b774a
Teemu <teemuikonen@live.fi>
2024-12-02 23:09:41 +0200

    Add project files.

---
+++ ViewModels/ListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using MauiProject.Models;
+
+namespace MauiProject.ViewModels
+{
+    internal partial class ListViewModel
+    {
+        public ImageView ImageView { get; set; }
+        public ListViewModel()
+        {
+            ImageView imageView = new ImageView();
+            imageView.Title = "Image 1";
+            imageView.Path = "https://i.imgur.com/JHjgOi6.jpeg";
+            ImageView = imageView;
+        }
+
+        [RelayCommand]
+        public async Task BackBtn()
+        {
+            await Shell.Current.GoToAsync("//MainPage");
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiProject.ViewModels
+{
+    internal partial class MainViewModel
+    {
+        [RelayCommand]
+        public async Task ListViewBtn()
+        {
+            await Shell.Current.GoToAsync("//ListView");
+        }
+    }
+}
